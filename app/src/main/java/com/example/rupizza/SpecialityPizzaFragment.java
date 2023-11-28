aadbb25f2bc7b4c5269bd5ab31fb15009b64323b
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-11-27 20:55:51 -0500

    project files

--- app/src/main/java/com/example/rupizza/SpecialityPizzaFragment.java
+++ app/src/main/java/com/example/rupizza/SpecialityPizzaFragment.java
@@ -1,4 +1,31 @@
 package com.example.rupizza;
 
-public class SpecialityPizzaFragment {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpecialityPizzaFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speciality_pizza); // Set the content view to your fragment layout
+
+        Button backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to start MainActivity
+                Intent intent = new Intent(SpecialityPizzaFragment.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
+
