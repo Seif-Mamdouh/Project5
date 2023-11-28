b218561ef27c6656d27e27bc9eac7bfd8c07e2df
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-11-28 12:15:05 -0500

    added actvites instead of regular classes

--- app/src/main/java/com/example/rupizza/MainActivity.java
+++ app/src/main/java/com/example/rupizza/MainActivity.java
@@ -15,12 +15,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonSpecialityPizza = findViewById(R.id.button);
+        Button buildYourOwn = findViewById(R.id.button2);
+        Button currentOrder = findViewById(R.id.button3);
+        Button storeOrder = findViewById(R.id.button4);
 
         buttonSpecialityPizza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Add your intent code here
                 Intent intent = new Intent(MainActivity.this, Speciality_Pizza.class);
+                startActivity(intent);
+            }
+        });
+
+        buildYourOwn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your intent code here
+                Intent intent = new Intent(MainActivity.this, BuildYourOwn.class);
+                startActivity(intent);
+            }
+        });
+
+        currentOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your intent code here
+                Intent intent = new Intent(MainActivity.this, CurrentOrder.class);
+                startActivity(intent);
+            }
+        });
+
+        storeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your intent code here
+                Intent intent = new Intent(MainActivity.this, StoreOrders.class);
                 startActivity(intent);
             }
         });
