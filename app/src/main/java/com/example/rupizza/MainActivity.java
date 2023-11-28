661fb82ad889dd4def7286d67e22a7b9455f0ab5
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-11-28 11:59:55 -0500

    modifications

--- app/src/main/java/com/example/rupizza/MainActivity.java
+++ app/src/main/java/com/example/rupizza/MainActivity.java
@@ -15,49 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonSpecialityPizza = findViewById(R.id.button);
-        Button buttonBuildYourOwn = findViewById(R.id.button2);
-        Button buttonCurrentOrder = findViewById(R.id.button3);
-        Button buttonStoreOrder = findViewById(R.id.button4);
 
         buttonSpecialityPizza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Inflate the Speciality Pizza layout dynamically
-                View specialityPizzaView = LayoutInflater.from(MainActivity.this).inflate(R.layout.speciality_pizza, null);
-                setContentView(specialityPizzaView);
+                // Add your intent code here
+                Intent intent = new Intent(MainActivity.this, Speciality_Pizza.class);
+                startActivity(intent);
             }
         });
-
-
-        buttonBuildYourOwn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Inflate the Speciality Pizza layout dynamically
-                View buildYourOwnPizza = LayoutInflater.from(MainActivity.this).inflate(R.layout.build_your_own_pizza, null);
-                setContentView(buildYourOwnPizza);
-            }
-        });
-
-
-        buttonCurrentOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Inflate the Speciality Pizza layout dynamically
-                View currentOrder = LayoutInflater.from(MainActivity.this).inflate(R.layout.current_order, null);
-                setContentView(currentOrder);
-            }
-        });
-
-
-        buttonStoreOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Inflate the Speciality Pizza layout dynamically
-                View storeOrder = LayoutInflater.from(MainActivity.this).inflate(R.layout.store_order, null);
-                setContentView(storeOrder);
-            }
-        });
-
-
     }
 }
